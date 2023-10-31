3c942e5f87496edfb31777de1d59b4f1b00af681
ruslankotliar <ruslankot101@gmail.com>
2023-10-31 23:24:37 +0100

    Force dynamic pages

--- src/app/[lang]/[country]/cars/_components/CatalogComponent.tsx
+++ src/app/[lang]/[country]/cars/_components/CatalogComponent.tsx
@@ -1,32 +1,10 @@
-import { CarDocument, Params, SearchParams } from '@/types';
+import { CarDocument } from '@/types';
 import { CarComponent } from './CarComponent';
-import { generateFetchURL } from '@/helpers';
 
-const fetchCars = async (searchParams: SearchParams, params: Params) => {
-  try {
-    const fetchURL = generateFetchURL('/cars', params, searchParams);
-    const response = await fetch(fetchURL);
-
-    const data = await response.json();
-
-    return data[0];
-  } catch (error) {
-    if (error instanceof Error) console.error(error.message);
-  }
-};
-
-export async function CatalogComponent({
-  searchParams,
-  params
-}: {
-  searchParams: SearchParams;
-  params: Params;
-}) {
-  const cars = await fetchCars(searchParams, params);
-
+export async function CatalogComponent({ cars }: { cars: CarDocument[] }) {
   return (
     <section className="flex flex-row flex-wrap justify-between gap-10 p-10 bg-gray-100 rounded-lg shadow-xl h-full">
-      {cars?.data.map((car: CarDocument) => (
+      {cars?.map((car: CarDocument) => (
         <CarComponent key={car._id + ''} car={car} />
       ))}
     </section>
